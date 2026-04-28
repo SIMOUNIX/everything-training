a63b89d2cecdf9f180cba278289a7982dcba81ab
SIMOUNIX <simounixx@gmail.com>
2026-04-28 19:19:25 +0200

    feat(fastmcp): start experimenting

---
+++ src/fastmcp/simple/my_client.py
@@ -0,0 +1,14 @@
+import asyncio
+
+from fastmcp import Client
+
+client = Client("my_server.py")  # connect using stdio
+
+
+async def call_tool(name: str):
+    async with client:
+        result = await client.call_tool("greet", {"name": name})
+        print(result)
+
+
+asyncio.run(call_tool("Ford"))
